7beea174b589c9c6d268deaea1bc8af0da657203
Kseniia Säkki <kssakk@utu.fi>
2021-12-20 20:34:27 +0200

    React app with authetication part

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Authentication from './components/Authentication'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isGuest:true,
+      user_login:'',
+      user_name:'',
+      user_token:'',
+      user_roles:[]      
+    }
+  }
+  componentDidMount() {
+    //check user_token
+  }
+
+  render() {   
+    return (
+      <div>
+        <Authentication/>
+        <h2>GDPR detecting system</h2>
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ src/components/Authentication.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Authentication =(props)=>{
+  
+    return(
+      <div>
+     <button>Join now</button>
+     <button>Sign in</button>
+      </div>
+    )
+  }
+export default Authentication
